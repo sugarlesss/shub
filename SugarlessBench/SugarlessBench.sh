efeadd6a96548a25ba1e752fb934a48fe793e288
“Sugarless” <jaded@foxmail.com>
2021-12-22 10:50:22 +0800

    update SugarlessBench/SugarlessBench.sh

--- SugarlessBench/SugarlessBench.sh
+++ SugarlessBench/SugarlessBench.sh
@@ -256,8 +256,13 @@ GetArch(){
 # 操作系统信息
 GetOperatingSystemInfo() {
     [ -f /etc/redhat-release ] && awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release && return
+    # cat /etc/redhat-release | awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release
+
     [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
+    # cat /etc/os-release | awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release
+
     [ -f /etc/lsb-release ] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
+    # cat /etc/lsb-release | awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release
 }
 
 # 将硬盘空间单位转换为 G, 并累加起来
